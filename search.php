508ecc00ea6b8ad2268272563dc6239ade87769c
Emmanuel Di Pretoro <edipretoro@gmail.com>
2016-11-19 22:57:07 +0100

    Adding a basic page to display the results

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php
   $context  = stream_context_create(array('http' => array('header' => 'Accept: application/xml')));
-  $url_query = 'http://gallica.bnf.fr/SRU?operation=searchRetrieve&version=1.2&query=dc.type%20any%20image%20and%20gallica%20any%20"' . $_POST['query'] . '"';
+  $url_query = 'http://gallica.bnf.fr/SRU?operation=searchRetrieve&version=1.2&query=dc.type%20any%20image%20and%20gallica%20any%20"' . $_GET['query'] . '"';
 	$xml = file_get_contents( $url_query , false, $context);
   $xml = simplexml_load_string($xml);
 
@@ -10,7 +10,49 @@
 <html>
 <head>
   <title></title>
+<style>
+* { box-sizing: border-box; }
 
+body {
+  padding-top: 50px;
+}
+
+.starter-template {
+  padding: 40px 15px;
+}
+
+.grid {
+  max-width: 1200px;
+}
+
+/* clearfix */
+.grid:after {
+  content: '';
+  display: block;
+  clear: both;
+}
+
+/* ---- grid-item ---- */
+
+.grid-sizer,
+.grid-item {
+  width: 20%;
+}
+
+.grid-item {
+  height: 120px;
+  float: left;
+  border-radius: 5px;
+}
+
+.grid-item--width2 { width:  40%; }
+.grid-item--width3 { width:  60%; }
+
+.grid-item--height2 { height: 200px; }
+.grid-item--height3 { height: 260px; }
+.grid-item--height4 { height: 360px; }
+
+</style>
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
 
@@ -39,22 +81,26 @@
 
 <div class="row">
 <div>
-<form action="/search.php" method="post">
-	<input type="text" id="query" name="query" class="form-control input-sm" maxlength="64" placeholder="Chercher une image" value="<?php echo $_POST['query'] ?>" />
+<form action="/search.php" method="get">
+	<input type="text" id="query" name="query" class="form-control input-sm" maxlength="64" placeholder="Chercher une image" value="<?php echo $_GET['query'] ?>" />
 	<button type="submit" class="btn btn-primary btn-sm">Search</button>
 </form>
 
 </div>
 
 <div>
+<div class="grid">
+<div class="grid-sizer"></div>
 <?php
   foreach ($xml->xpath('//dc:identifier') as $identifier) {
 	  if ( preg_match( '/gallica.bnf.fr/', $identifier ) ) {
-      echo $identifier[0];
-  		echo '<br />';
+?>
+<div class="grid-item">
+<a href="<?php echo $identifier[0] ?>/f1.highres" target="_blank"><img src="<?php echo $identifier[0] ?>.thumbnail" /></a>
+</div>
+<?php
 		}
   }
-
 ?>
 </div>
 
@@ -64,6 +110,15 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="
   sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
 <script src="/static/scripts/meme.js"></script>
+<script src="https://unpkg.com/masonry-layout@4.1/dist/masonry.pkgd.min.js"></script>
+	<script>
+		$('.grid').masonry({
+		// options
+		  itemSelector: '.grid-item',
+		  columnWidth: '.grid-sizer',
+			percentPosition: true
+		});
+	</script>
 
 </body>
 </html>
