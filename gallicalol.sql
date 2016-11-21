aa63e4e36fbbd3b75e525ccd051c10d835f1a51d
tam kien duong <tk@deveha.com>
2016-11-21 13:06:36 +0100

    cleaning

--- gallicalol.sql
+++
@@ -1,9 +0,0 @@
-create table memes (
- id integer not null primary key,
- gallica_url text not null,
- top_text text,
- bottom_text text,
- scale real not null,
- image blob not null,
- clicked integer not null default 0
-);
