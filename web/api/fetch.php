beaf2dabfc258c9915f3b6f646d2bf1e6cca80d4
Alexandre Segura <mex.zktk@gmail.com>
2016-11-26 17:04:04 +0100

    Add caching on API requests.

--- web/api/fetch.php
+++ web/api/fetch.php
@@ -1,5 +1,10 @@
 <?php
-header('Content-Type: application/json');
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Doctrine\Common\Cache\FilesystemCache;
+
+$cache = new FilesystemCache(__DIR__ .'/../../cache');
 
 if ( !empty($_GET['query']) ) {
     $query = $_GET['query'];
@@ -22,38 +27,45 @@ $parameters = [
   "query" => "dc.type any image and gallica any ".urldecode($query)
 ];
 
-$q = $api_endpoint."?".http_build_query($parameters);
+$cacheKey = http_build_query($parameters);
 
-$xml = file_get_contents( $q , false, $context);
-$xml = simplexml_load_string($xml);
+if (!$response = $cache->fetch($cacheKey)) {
 
-$results = [];
+    $q = $api_endpoint."?".http_build_query($parameters);
 
-foreach ($xml->xpath('//dc:identifier') as $identifier) {
-  $identifier = (string) $identifier;
-  // @link http://www.bnf.fr/fr/professionnels/issn_isbn_autres_numeros/a.ark.html
-  if ( preg_match( '#^https?://gallica.bnf.fr/(ark:.*)#', $identifier, $matches ) ) {
+    $xml = file_get_contents( $q , false, $context);
+    $xml = simplexml_load_string($xml);
 
-    $ark_id = $matches[1];
+    $results = [];
 
-    $r = [
-      "id" => $ark_id,
-      "thumb" => $identifier."/lowres",
-      "img" => $identifier."/f1.highres"
+    foreach ($xml->xpath('//dc:identifier') as $identifier) {
+        $identifier = (string) $identifier;
+        // @link http://www.bnf.fr/fr/professionnels/issn_isbn_autres_numeros/a.ark.html
+        if ( preg_match( '#^https?://gallica.bnf.fr/(ark:.*)#', $identifier, $matches ) ) {
+
+            $ark_id = $matches[1];
+
+            $r = [
+              "id" => $ark_id,
+              "thumb" => $identifier."/lowres",
+              "img" => $identifier."/f1.highres"
+            ];
+
+            array_push($results, $r);
+        }
+    }
+
+    $records = $xml->xpath("//srw:numberOfRecords[1]/text()")[0]->__toString();
+
+    $response = [
+      "page" => $parameters["page"],
+      "records" => $records,
+      "results" => $results
     ];
 
-    array_push($results, $r);
-	}
+    // Cache for 10 minutes
+    $cache->save($cacheKey, $response, 60 * 10);
 }
 
-$records = $xml->xpath("//srw:numberOfRecords[1]/text()")[0]->__toString();
-
-$response = [
-  "page" => $parameters["page"],
-  "records" => $records,
-  "results" => $results
-];
-
+header('Content-Type: application/json');
 echo(json_encode($response));
-
-?>
