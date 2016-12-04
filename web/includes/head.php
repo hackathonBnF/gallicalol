0e2031c1f538f3ce6a4563b4b4c0a936b67eff2b
Alexandre Segura <mex.zktk@gmail.com>
2016-12-04 18:37:22 +0100

    Remove useless file.

--- web/includes/head.php
+++
@@ -1,6 +0,0 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Gallica.lol : mets du LOL dans ton Gallica</title>
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
-<script src="https://use.fontawesome.com/354985ad2d.js"></script>
-<link rel="stylesheet" type="text/css" href="/static/styles/main.css">
