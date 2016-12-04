35155fbcacfa359e79ac84ead0d9f6c4267a3d74
Alexandre Segura <mex.zktk@gmail.com>
2016-12-04 20:06:08 +0100

    Add script to create image on filesystem.

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -6,3 +6,16 @@ function base64_to_bin($base64) {
 
     return base64_decode($data);
 }
+
+// @link http://stackoverflow.com/questions/191845/how-to-store-images-in-your-filesystem
+function int_to_filepath($id) {
+    $dir = strrev((string) $id);
+
+    if (strlen($dir) % 2 !== 0) {
+        $dir = str_pad($dir, (strlen($dir) + 1), "0", STR_PAD_LEFT);
+    }
+
+    $parts = str_split($dir, 2);
+
+    return implode('/', $parts);
+}
